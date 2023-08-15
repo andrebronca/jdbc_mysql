4827f3f55548350911011a51b92d6a16246a8e71
André R. B <bronca.andre@gmail.com>
2023-08-14 21:06:48 -0300

    Criação de uma classe com annotations, alteração da classe que retorna um session. Mudança do cfg.xml para mapear a classe

--- src/com/livro/capitulo3/hibernate/conexao/HibernateUtil.java
+++ src/com/livro/capitulo3/hibernate/conexao/HibernateUtil.java
@@ -1,11 +1,14 @@
 package com.livro.capitulo3.hibernate.conexao;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
+	private static final SessionFactory sessionFactoryAnnotation = buildSessionFactoryAnnotation();
 	
+	//os dois métodos abaixo fazem uso do mapeamento via hbm.xml
 	private static SessionFactory buildSessionFactory() {
 		try {
 			Configuration cfg = new Configuration();
@@ -19,5 +22,23 @@ public class HibernateUtil {
 	
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
+	}
+	
+	
+	//refiz esses métodos para não desativar o métodos acima
+	//aqui faz uso de annotations
+	private static SessionFactory buildSessionFactoryAnnotation() {
+		try {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+		} catch (Throwable e) {
+			System.err.println("Criação inicial do objeto SessionFactory falhou. Erro: "+ e.getMessage());
+			throw new ExceptionInInitializerError();
+		}
+	}
+	
+	public static SessionFactory getSessionFactoryAnnotation() {
+		return sessionFactoryAnnotation;
 	}
 }
